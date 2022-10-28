a9636124532ca9eba016442cb42a59afbf782945
kuani <kuanimike@gmail.com>
2022-10-28 09:20:07 +0800

    add daily calculate controller

---
+++ Fitness_NoTwo/DailyCalculation.swift
@@ -0,0 +1,18 @@
+//
+//  DailyCalculation.swift
+//  Fitness_NoTwo
+//
+//  Created by kuani on 2022/10/28.
+//
+
+import Foundation
+
+struct DailyCalculation{
+    
+    let height:String
+    let weight:String
+    let gender:String
+    let bmi:String
+    let standardvalue:String
+    
+}
